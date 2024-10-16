77d9b769a6e262558f14378f3b7cfac7deee9944
Michael <mikerobards@icloud.com>
2024-10-16 15:01:19 -0400

    add tools

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 import './style.css'
 import OpenAI from 'openai'
+import { getCurrentWeather, getLocation } from './tools'
 
 export const openai = new OpenAI({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY,
   dangerouslyAllowBrowser: true
 })
 
-// test
-
+const weather = await getCurrentWeather()
+const location = await getLocation()
 
 const response = await openai.chat.completions.create({
   model: "gpt-4o",
   messages: [
     {
       role: "user",
-      content: "Give me a list of activity ideas based on my current location and weather."
+      content: `Give me a list of activity ideas based on my current location of ${location} and weather of ${weather}.`
     }
   ]
 })
