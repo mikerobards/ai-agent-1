551222c202e87c19d72cdbfa4ef4975307e4985a
Michael <mikerobards@icloud.com>
2024-10-17 10:59:29 -0400

    add agent function

--- main.js
+++ main.js
@@ -20,6 +20,8 @@ export const openai = new OpenAI({
  * 4. End condition - final Answer is given
  */
 
+
+
 const systemPrompt = `
 You cycle through Thought, Action, PAUSE, Observation. At the end of the loop you output a final Answer. 
 Your final answer should be highly specific to the observations you have from running
@@ -34,7 +36,7 @@ Available actions:
     E.g. getCurrentWeather: Salt Lake City
     Returns the current weather of the location specified.
 - getLocation:
-    E.g. getLocation: null
+    E.g. getLocation: null 
     Returns user's location details. No arguments needed.
 
 Example session:
@@ -58,20 +60,29 @@ You then output:
 Answer: <Suggested activities based on sunny weather that are highly specific to New York City and surrounding areas.>
 `
 
+async function agent(query) {
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: systemPrompt
+      },
+      {
+        role: "user",
+        content: query
+      }
+    ]
+  })
+  const chatgpt = response.choices[0].message.content
+
+  document.getElementById("text-box").textContent = chatgpt
+
+  console.log(chatgpt)
+}
+
+agent('What is the current weather in Kennesaw, Georgia?')
 
 
-// const response = await openai.chat.completions.create({
-//   model: "gpt-3.5-turbo",
-//   messages: [
-//     {
-//       role: "user",
-//       content: `Give me a list of activity ideas based on my current location of ${location} and weather of ${weather}.`
-//     }
-//   ]
-// })
 
-const chatgpt = response.choices[0].message.content
-
-document.getElementById("text-box").textContent = chatgpt
-
-console.log(chatgpt)
